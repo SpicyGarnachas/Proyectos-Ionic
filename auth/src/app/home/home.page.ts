5b7a53f155410fd1187cadd23c28f468f4739b4b
Diego Martínez <56555056+SpicyGarnachas@users.noreply.github.com>
2022-11-06 16:28:25 +0000

    Added auth

---
+++ auth/src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { LoginPage } from '../login/login.page';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public modalController: ModalController) {
+    this.iniciarSesion();
+  }
+
+  async iniciarSesion() {
+    const modal = await this.modalController.create({
+      component: LoginPage
+    });
+    return await modal.present();
+  }
+
+}
---
+++ auth/src/app/login/login.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  usuario: string;
+  contrasena: string;
+
+
+  constructor(public loginCtrl: ModalController, public alertController: AlertController ) { }
+
+  ngOnInit() {
+  }
+
+  async entrar() {
+    if (this.usuario === 'admin' && this.contrasena === 'admin') {
+      this.loginCtrl.dismiss();
+    } else {
+      const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Inicio de sesión',
+      message: 'No se encontro este usuario',
+      buttons: ['OK']
+  });
+
+      await alert.present();
+
+}
+  }
+}
